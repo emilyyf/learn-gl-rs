55ef224c7f5244fda488697c332686d8886ed249
Emily Froeschlin <emilyfroe@tutanota.com>
2021-01-10 00:06:22 -0300

    Refactored some code

--- hello-triangle/src/main.rs
+++ hello-triangle/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(destructuring_assignment)]
+
 extern crate gl;
 extern crate glfw;
 use std::ffi::CString;
@@ -5,6 +7,80 @@ use std::{mem, ptr};
 
 use crate::glfw::Context;
 use gl::types::*;
+
+fn load_shader(source: std::string::String, _type: GLenum) -> u32 {
+	let shader;
+	let c_str = CString::new(source.as_bytes()).unwrap();
+	unsafe {
+		shader = gl::CreateShader(_type);
+		gl::ShaderSource(shader, 1, &c_str.as_ptr(), ptr::null());
+		gl::CompileShader(shader);
+	}
+	shader
+}
+
+fn compile_program(vertex_shader: u32, fragment_shader: u32) -> u32 {
+	let program;
+	unsafe {
+		program = gl::CreateProgram();
+
+		gl::AttachShader(program, vertex_shader);
+		gl::AttachShader(program, fragment_shader);
+
+		gl::LinkProgram(program);
+
+		gl::DeleteShader(vertex_shader);
+		gl::DeleteShader(fragment_shader);
+	}
+	program
+}
+
+fn prepare_gl() -> (u32, u32) {
+	let vertices: [f32; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.5, 0.5, 0.0];
+
+	let vs_src = std::fs::read_to_string("triangle.vert")
+		.expect("Error while reading vertex shader");
+	let fs_src = std::fs::read_to_string("triangle.frag")
+		.expect("Error while reading fragment shader");
+
+	let vertex_shader = load_shader(vs_src, gl::VERTEX_SHADER);
+	let fragment_shader = load_shader(fs_src, gl::FRAGMENT_SHADER);
+	let shader_program = compile_program(vertex_shader, fragment_shader);
+
+	let mut vao: u32 = 0;
+	let mut vbo: u32 = 0;
+
+	unsafe {
+		gl::GenVertexArrays(1, &mut vao);
+		gl::BindVertexArray(vao);
+
+		gl::GenBuffers(1, &mut vbo);
+		gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+		gl::BufferData(
+			gl::ARRAY_BUFFER,
+			(vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+			mem::transmute(&vertices[0]),
+			gl::STATIC_DRAW,
+		);
+
+		gl::UseProgram(shader_program);
+
+		gl::VertexAttribPointer(
+			0,
+			3,
+			gl::FLOAT,
+			gl::FALSE,
+			3 * mem::size_of::<GLfloat>() as GLsizei,
+			ptr::null(),
+		);
+		gl::EnableVertexAttribArray(0);
+
+		gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+		gl::BindVertexArray(0);
+	}
+
+	(vao, shader_program)
+}
 
 fn main() {
 	let mut _glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
@@ -26,70 +102,10 @@ fn main() {
 
 	gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-	let vertices: [f32; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.5, 0.5, 0.0];
-
-	let mut vbo = 0;
-	let mut vao = 0;
-	let mut vertex_shader = 0;
-	let mut fragment_shader = 0;
-	let mut shader_program = 0;
-	let vs_src: &str = "#version 330 core
-		layout (location = 0) in vec3 aPos;
-		void main()
-		{
-				gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-		}";
-	let fs_src: &str = "#version 330 core
-		out vec4 FragColor;
-		void main()
-		{
-				FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-		}";
-
-	unsafe {
-		gl::GenVertexArrays(1, &mut vao);
-		gl::BindVertexArray(vao);
-
-		gl::GenBuffers(1, &mut vbo);
-		gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-		gl::BufferData(
-			gl::ARRAY_BUFFER,
-			(vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-			mem::transmute(&vertices[0]),
-			gl::STATIC_DRAW,
-		);
-
-		let c_str = CString::new(vs_src.as_bytes()).unwrap();
-		vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
-		gl::ShaderSource(vertex_shader, 1, &c_str.as_ptr(), ptr::null());
-		gl::CompileShader(vertex_shader);
-
-		let c_str2 = CString::new(fs_src.as_bytes()).unwrap();
-		fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
-		gl::ShaderSource(fragment_shader, 1, &c_str2.as_ptr(), ptr::null());
-		gl::CompileShader(fragment_shader);
-
-		shader_program = gl::CreateProgram();
-
-		gl::AttachShader(shader_program, vertex_shader);
-		gl::AttachShader(shader_program, fragment_shader);
-
-		gl::LinkProgram(shader_program);
-		gl::UseProgram(shader_program);
-
-		gl::VertexAttribPointer(
-			0,
-			3,
-			gl::FLOAT,
-			gl::FALSE,
-			3 * mem::size_of::<GLfloat>() as GLsizei,
-			ptr::null(),
-		);
-		gl::EnableVertexAttribArray(0);
-
-		gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-		gl::BindVertexArray(0);
-	}
+	let vao: u32;
+	let shader_program: u32;
+	
+	(vao, shader_program) = prepare_gl();
 
 	while !window.should_close() {
 		for (_, event) in glfw::flush_messages(&events) {
@@ -117,10 +133,5 @@ fn main() {
 
 		_glfw.poll_events();
 		window.swap_buffers();
-	}
-
-	unsafe {
-		gl::DeleteShader(vertex_shader);
-		gl::DeleteShader(fragment_shader);
 	}
 }
